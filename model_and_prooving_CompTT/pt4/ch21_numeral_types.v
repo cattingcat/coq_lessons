bbecf24a211c9828627ceb39fc5a73d5b3aa0cfd
Mark Martynov <mark.martynov@juspay.in>
2022-09-04 20:51:35 +0300

    pt4

---
+++ model_and_prooving_CompTT/pt4/ch21_numeral_types.v
@@ -0,0 +1,78 @@
+From mathcomp Require Import all_ssreflect.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Inductive numeral:  nat -> Type :=
+  | Z: forall n, numeral n.+1
+  | U: forall n, numeral n -> numeral n.+1.
+
+Print numeral_ind.
+
+Definition numeral_ind2 
+  (P : forall n : nat, numeral n -> Prop)
+  (fz : forall n : nat, P n.+1 (Z n))
+  (fu : forall (n : nat) (n' : numeral n), P n n' -> P n.+1 (U n'))
+  := 
+    fix F (n: nat) (n': numeral n) {struct n'} : P n n' :=  
+      match n' as n2 in (numeral n1) return (P n1 n2) with
+      | Z n1 => fz n1
+      | @U n1 n2 => fu n1 n2 (F n1 n2)
+      end.
+
+
+Definition P: forall n, numeral n.+1 -> option (numeral n) :=
+  fun n num =>
+    match num with
+    | Z n' => None
+    | U _ n' => Some n'
+    end.
+
+Print Empty_set.
+Print void.
+Definition P': forall n, numeral n -> match n with
+                                      | 0 => void
+                                      | S n' => option (numeral n')
+                                      end :=
+  fun _ num =>
+    match num with
+    | Z n' => None
+    | U _ n' => Some n'
+    end.
+
+Definition P2: forall n, numeral n.+1 -> option (numeral n) := 
+  fun n num => @P' n.+1 num.
+
+Definition P'': forall n, numeral n.+2 -> numeral n.+1 :=
+  fun n num => 
+    match @P' n.+2 num with
+    | Some a => a
+    | None => Z n
+    end.
+
+(* 21.2.1 *)
+Lemma constr_disj: forall n (a: numeral n), @Z n <> @U n a.
+Proof.
+  by move => n a H.
+Qed.
+Print constr_disj.
+
+(* 21.2.2 *)
+Lemma num0: numeral 0 -> void.
+Proof.
+  by apply P'.
+Qed.
+
+(* 21.2.3 *)
+Fixpoint tst (n m: nat): numeral n.+1 :=
+  match n, m return numeral n.+1 with
+  | S n', 0 => Z n'.+1
+  | S n', S m' => @U n.+1 (tst n' m')
+  | 0, S m' => Z 0
+  | 0, 0 => Z 0
+  end.
+
+Definition into_numeral: forall (n: nat), nat -> numeral n.
+Proof.
+
+  
